50bb5ee69273d848b6f99198a26c8a9ef424670a
jahup <jahup@qq.com>
2023-01-30 20:56:33 +0800

    2023.1.30

---
+++ SSM/Integration/src/main/java/org/example/service/BookServiceImpl.java
@@ -0,0 +1,33 @@
+package org.example.service;
+
+import org.example.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements bookService{
+
+    @Override
+    public int addBook(Books books) {
+        return 0;
+    }
+
+    @Override
+    public int deleteBookById(int id) {
+        return 0;
+    }
+
+    @Override
+    public int updateBook(Books books) {
+        return 0;
+    }
+
+    @Override
+    public Books queryBookById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Books> queryAllBook() {
+        return null;
+    }
+}
--- SSM/Integration/src/main/java/org/example/service/bookService.java
+++ SSM/Integration/src/main/java/org/example/service/bookService.java
@@ -1,8 +1,18 @@
 package org.example.service;
 
 import org.example.dao.BookMapper;
+import org.example.pojo.Books;
+
+import java.util.List;
 
 public interface bookService {
+    public int addBook(Books books);
+    public int deleteBookById(int id);
 
+    public int updateBook(Books books);
+
+    public Books queryBookById(int id);
+
+    public List<Books> queryAllBook();
 
 }
