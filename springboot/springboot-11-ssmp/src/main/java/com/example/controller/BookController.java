b62b17486e004b214e2b314bc0fdf938ffadb07c
jahup <572598394@qq.com>
2023-02-09 21:33:09 +0800

    2023.2.9

--- springboot/springboot-11-ssmp/src/main/java/com/example/controller/BookController.java
+++ springboot/springboot-11-ssmp/src/main/java/com/example/controller/BookController.java
@@ -15,28 +15,51 @@ import java.util.List;
 public class BookController {
     @Autowired
     private IBookService iBookService;
+
     @GetMapping("/select")
-    public R getAllBook(){
-        return new R(true,iBookService.list());
+    public R getAllBook() {
+        return new R(true, iBookService.list());
     }
 
-    @PostMapping("save")
-    public R saveBook(books book){
-        return new R(iBookService.save(book),null);
+    @PostMapping("/save")
+    public R saveBook(@RequestBody books book) {
+
+        books Id = iBookService.getById(book.getBookID());
+        if (Id!=null ) {
+            boolean b = iBookService.updateById(book);
+            return new R(b,null);
+        } else {
+            System.out.println("------------------------" + book.toString());
+            return new R(iBookService.save(book), null);
+
+        }
     }
 
     @GetMapping("/select/{id}")
-    public R getBookById(@PathVariable Integer id){
-        return new R(true,iBookService.getById(id));
+    public R getBookById(@PathVariable Integer id) {
+        return new R(true, iBookService.getById(id));
     }
 
     @GetMapping("/select/{a}/{b}")
-    public R getPage(@PathVariable("a") int CurrentPage,@PathVariable("b") int PageSize){
+    public R getPage(@PathVariable("a") int CurrentPage, @PathVariable("b") int PageSize) {
         IPage<books> page = iBookService.getPage(CurrentPage, PageSize);
-        return new R(true,page);
+        return new R(true, page);
 
     }
 
+    @DeleteMapping("/delete/{name}")
+    public R deleteByName(@PathVariable("name") String name) {
+        books bookByName = iBookService.getBookByName(name);
+        System.out.println(bookByName.toString());
+        boolean b = iBookService.removeById(bookByName.getBookID());
+        return new R(b, null);
+
+    }
+    @PostMapping("/updata")
+    public R updata(@RequestBody books book){
+        boolean b = iBookService.updateById(book);
+        return new R(b,null);
+    }
 
 
 }
