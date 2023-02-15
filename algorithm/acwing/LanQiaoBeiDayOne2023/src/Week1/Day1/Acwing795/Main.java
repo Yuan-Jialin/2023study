343609b9e091d5983dbb342e59b025f0a5d919c8
jahup <572598394@qq.com>
2023-02-15 21:33:54 +0800

    2023.2.15

--- algorithm/acwing/LanQiaoBeiDayOne2023/src/Week1/Day1/Acwing795/Main.java
+++ algorithm/acwing/LanQiaoBeiDayOne2023/src/Week1/Day1/Acwing795/Main.java
@@ -1,7 +1,7 @@
 package Week1.Day1.Acwing795;
 
-import javax.security.sasl.SaslClient;
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.IntFunction;
 
 public class Main {
 
@@ -23,3 +23,76 @@ public class Main {
 
     }
 }
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null)
+            return new ArrayList<>();
+
+        List<List<Integer>> ans = new ArrayList<>();
+        Stack<TreeNodePlus>s=new Stack<>();
+        Stack<TreeNodePlus>p=new Stack<>();
+        List<Integer>v=new ArrayList<>();
+        int f=1;
+        s.add(new TreeNodePlus(root,1));
+        while (!s.isEmpty()){
+            if(s.isEmpty())
+            {
+                s=p;
+                for (TreeNodePlus treeNodePlus : p) {
+                    System.out.printf(String.valueOf(treeNodePlus.treeNode.val));
+                }
+                for (TreeNodePlus treeNodePlus : s) {
+                    System.out.printf(String.valueOf(treeNodePlus.treeNode.val));
+                }
+                p=new Stack();
+            }
+
+            TreeNodePlus pop = s.pop();
+            if(pop.f!=f){
+                ans.add(v);
+                v=new ArrayList<>();
+                f++;
+            }
+            v.add(pop.treeNode.val);
+            if(f%2==1){
+                if(pop.treeNode.left!=null)
+                    s.add(new TreeNodePlus(pop.treeNode.left,f+1));
+                if(pop.treeNode.right!=null)
+                    s.add(new TreeNodePlus(pop.treeNode.right,f+1));
+            }
+            else{
+                if(pop.treeNode.right!=null)
+                    s.add(new TreeNodePlus(pop.treeNode.right,f+1));
+                if(pop.treeNode.left!=null)
+                    s.add(new TreeNodePlus(pop.treeNode.left,f+1));
+            }
+
+
+        }
+
+return ans;
+    }
+}
+
+class TreeNodePlus {
+
+    TreeNode treeNode;
+    int f;
+
+    public TreeNodePlus(TreeNode treeNode, int f) {
+        this.treeNode = treeNode;
+        this.f = f;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
